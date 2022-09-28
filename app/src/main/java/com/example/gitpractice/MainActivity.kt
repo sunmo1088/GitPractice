0b7efd0e142a16ac111fc9ead066dd289982567d
Sungjin Mo <samuelmo78@gmail.com>
2022-09-27 22:17:06 -0600

    commit practice1

--- app/src/main/java/com/example/gitpractice/MainActivity.kt
+++ app/src/main/java/com/example/gitpractice/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
 
         println("This is contained in my first commit")
         println("Changed from second user")
+        println("commit practice")
 
     }
 }
